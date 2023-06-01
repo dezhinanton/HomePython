bc6dfeb8c075b78d667ffd5fc168ac33bc70c86e
Dezhin Anton <dezhin@email.com>
2023-06-01 12:40:54 +0300

    Task22 Done

---
+++ Task22.py
@@ -0,0 +1,17 @@
+from random import randint
+n = int(input("Укажите размер массива: "))
+arr1 = []
+for i in range(n):
+    arr1.append(randint(1, 10))
+print(arr1)
+from random import randint
+m = int(input("Укажите размер массива: "))
+arr2 = []
+for i in range(m):
+    arr2.append(randint(1, 10))
+print(arr2)
+all_numbers = list()
+for num in arr1:
+    if num in arr2:
+        all_numbers.append(num)
+print(sorted(set(all_numbers)))
