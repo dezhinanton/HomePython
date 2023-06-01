69e30e177cf9373be87acaccf88eab2d81cba7d4
Dezhin Anton <dezhin@email.com>
2023-06-01 13:15:11 +0300

    Task24 refreshed

--- Task24.py
+++ Task24.py
@@ -4,8 +4,11 @@ arr = [random.randint(1, n) for i in range(n)]
 print(arr)
 i = 0
 max = 0
-while i < len(arr)-1:
-    if max < arr[i-1] + arr[i] + arr[i + 1]:
-        max = arr[i-1] + arr[i] + arr[i + 1]
-    i+=1
+for i in range(1, len(arr) - 1):
+   if max < arr[i - 1] + arr[i] + arr[i + 1]:
+      max = arr[i - 1] + arr[i] + arr[i + 1]
+if max < arr[0] + arr[1] + arr[len(arr) - 1]:
+   max = arr[0] + arr[1] + arr[len(arr) - 1]
+if max < arr[0] + arr[len(arr) - 1] + arr[len(arr) - 2]:
+   max = arr[0] + arr[len(arr) - 1] + arr[len(arr) - 2]
 print(max)
